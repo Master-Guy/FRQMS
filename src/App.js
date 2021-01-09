76f693077d7cf4537871c42c2bc3ed25bac1eb6b
Master-Guy <master.guy.marcel@gmail.com>
2021-01-09 20:04:12 +0100

    First ESLINT configuration

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import { hot } from 'react-hot-loader/root';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
 
-const app = function app() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react"
+import { hot } from "react-hot-loader/root"
+
+import logo from "./logo.svg"
+
+function app() {
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <img src={logo} className='App-logo' alt='logo' />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className='App-link'
+                    href='https://reactjs.org'
+                    target='_blank'
+                    rel='noopener noreferrer'>
+                    Learn React
+                </a>
+            </header>
+        </div>
+    )
 }
 
-export default hot(app);
+export default hot(app)
